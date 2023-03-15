cd0c886a0d033aee090f6a52933fd6f5044f0be9
etopritika <dimaprytyka@gmail.com>
2023-03-15 14:36:01 +0200

    init

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', startChangingColor);
+stopBtn.addEventListener('click', stopChangingColor);
+
+function startChangingColor() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  
+  startBtn.removeAttribute('enabled', 'enabled');
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled', 'disabled');
+  stopBtn.setAttribute('enabled', 'enabled');
+}
+
+function stopChangingColor() {
+  clearInterval(timerId);
+
+  stopBtn.removeAttribute('enabled', 'enabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled', 'disabled');
+  startBtn.setAttribute('enabled', 'enabled');
+}
