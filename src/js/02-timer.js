1dd8c8427c7c2ee4664f84fdd0bc287576c1fd64
etopritika <dimaprytyka@gmail.com>
2023-03-16 13:36:15 +0200

    init

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,13 +6,14 @@ import 'flatpickr/dist/flatpickr.min.css';
 const timeInput = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 const refs = {
-  days: document.querySelector("[data-days]"),
-  hours: document.querySelector("[data-hours]"),
-  minutes: document.querySelector("[data-minutes]"),
-  seconds: document.querySelector("[data-seconds]")
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
-
-console.log(refs.days);
+const currentDate = new Date();
+let timerId = null;
+let isActive = false;
 
 const options = {
   enableTime: true,
@@ -20,39 +21,67 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
-    setInterval(() => {
-      const currentTime = Date.now();
-      const timeLeft = selectedDates[0] - currentTime;
-      console.log(convertMs(timeLeft));
-    }, 1000);
+    if (selectedDates[0] < currentDate) {
+      window.alert('Please choose a date in the future');
+      return;
+    } else {
+      turnOnBtn(startClockFace());
+      function startClockFace() {
+        if (isActive) {
+          return;
+        }
+        isActive = true;
+
+        timerId = setInterval(() => {
+          const currentTime = Date.now();
+          const timeLeft = selectedDates[0] - currentTime;
+          const time = convertMs(timeLeft);
+          updateClockFace(time);
+          if (timeLeft < 999) {
+            clearInterval(timerId);
+          }
+        }, 1000);
+      }
+    }
   },
 };
 
 flatpickr(timeInput, options);
 
-
-function pad(value) {
+function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = pad(Math.floor(ms / day));
-    // Remaining hours
-    const hours = pad(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = pad(Math.floor((ms % day) / hour));
-    // Remaining seconds
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-  
-    return { days, hours, minutes, seconds };
-  }
-  
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+function turnOnBtn(startClockFace) {
+  startBtn.addEventListener('click', startClockFace);
+  startBtn.removeAttribute('disabled');
+  startBtn.setAttribute('enabled', 'enabled');
+}
