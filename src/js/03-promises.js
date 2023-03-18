2cc5c2892ac39ce291a2924e0a5f82a5c53bc6e4
etopritika <dimaprytyka@gmail.com>
2023-03-18 13:18:27 +0200

    init

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,11 +10,13 @@ form.addEventListener('submit', submitedForm);
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
 
@@ -30,12 +32,12 @@ function submitedForm(e) {
   for (let i = 0; i < amount; i += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-      position =+ 1;
+      position = position + 1;
       delay += step;
   }
 }
