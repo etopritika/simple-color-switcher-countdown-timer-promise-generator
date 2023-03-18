69d441cf059e8aa879ad6474642f79fdda8970a5
etopritika <dimaprytyka@gmail.com>
2023-03-18 12:41:22 +0200

    init

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,11 @@
 import Notiflix from 'notiflix';
 
-const formEl = document.querySelector('.form');
-const delayEl = document.querySelector('[name=delay]');
-console.log(delayEl);
-const stepEl = document.querySelector('[name=step]');
-const amountEl = document.querySelector('[name=amount]');
+const form = document.querySelector('.form');
+
 let timerId = null;
 let isActive = false;
 
-formEl.addEventListener('submit', submitedForm);
+form.addEventListener('submit', submitedForm);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -23,42 +20,22 @@ function createPromise(position, delay) {
 
 function submitedForm(e) {
   e.preventDefault();
-  const {
-    elements: { delay, step, amount },
-  } = e.target;
+  const formEl = e.currentTarget.elements;
+  const time = Number(formEl.delay.value);
+  const step = Number(formEl.step.value);
+  const amount = Number(formEl.amount.value);
 
-  const delayValue = delay.value;
-  console.log(delay.value);
-  const stepValue = step.value;
-  const amaountValue = amount.value;
-  promiseCreator(delayValue, stepValue, amaountValue);
-}
-
-function promiseCreator(delayValue, stepValue, amaountValue) {
-  let alertStep = 1;
-  console.log(delayValue);
-  setTimeout(() => {
-    if (isActive) {
-      return;
-    }
-    isActive = true;
-    timerId = setInterval(() => {
-      createPromise(alertStep, delayValue)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(
-            `✅ Fulfilled promise ${position} in ${delay}ms`
-          );
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(
-            `❌ Rejected promise ${position} in ${delay}ms`
-          );
-        });
-      alertStep = alertStep + 1;
-      delayValue = Number(delayValue) + Number(stepValue);
-      if (alertStep === Number(amaountValue) + 1) {
-        clearInterval(timerId);
-      }
-    }, stepValue);
-  }, delayValue);
+  let position = 1;
+  let delay = time;
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      position =+ 1;
+      delay += step;
+  }
 }
